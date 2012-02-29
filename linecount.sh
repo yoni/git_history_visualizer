6c57e464a75707c5bfa19759d72818941108a299
Yoni Ben-Meshulam <yoni@opower.com>
2012-02-29 10:00:50 -0500

    Dump to CSV onstead of stdout. Also dump the commits with timestamps as a separate CSV.

--- linecount.sh
+++ linecount.sh
@@ -3,20 +3,22 @@
 # Clones a git repository and runs line count analysis for each state of the repository.
 
 repository=$1
-out=$2
 temporary_repo='tmp.git'
 
 git clone $1 $temporary_repo
-cd $temporary_repo
 
-for commit in `git log --format='%H'`; do
+
+cd $temporary_repo
+git log --format="%H,%ci" > ../out/commits.csv
+for commit in `git log --format="%H"`; do
   git reset --hard $commit &&\
   R CMD build . &&\
   ../cloc-1.55.pl \
     --read-lang-def=../cloc.out \
     --extract-with="tar zxf >FILE<" \
     --csv \
-    *.tar.gz > $out/$commit.out &&\
+    --out=../out/$commit.csv \
+    *.tar.gz &&\
   rm *.tar.gz
 done
 cd -
