995a149ef709c5fffbb9bc09f86e67ff8d1258ee
Yoni Ben-Meshulam <yoni@opower.com>
2012-02-29 10:20:08 -0500

    Don't build and count R package lines, instead just cound all lines in the project directory.

--- linecount.sh
+++ linecount.sh
@@ -12,14 +12,11 @@ cd $temporary_repo
 git log --format="%H,%ci" > ../out/commits.csv
 for commit in `git log --format="%H"`; do
   git reset --hard $commit &&\
-  R CMD build . &&\
   ../cloc-1.55.pl \
     --read-lang-def=../cloc.out \
-    --extract-with="tar zxf >FILE<" \
     --csv \
     --out=../out/$commit.csv \
-    *.tar.gz &&\
-  rm *.tar.gz
+    *
 done
 cd -
 rm -rf $temporary_repo
