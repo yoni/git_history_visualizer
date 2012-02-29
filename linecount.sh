67dd1d4b26f9120f5a3ebd797fed7bd5d83ba2af
Yoni Ben-Meshulam <yoni@opower.com>
2012-02-28 19:26:49 -0500

    Augmented Tyler's line count script with a script that goes over the entire commit history of the project, running it for each individual commit.

---
+++ linecount.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+# Clones a git repository and runs line count analysis for each state of the repository.
+
+repository=$1
+out=$2
+temporary_repo='tmp.git'
+
+git clone $1 $temporary_repo
+cd $temporary_repo
+
+for commit in `git log --format='%H'`; do
+  git reset --hard $commit &&\
+  R CMD build . &&\
+  ../cloc-1.55.pl \
+    --read-lang-def=../cloc.out \
+    --extract-with="tar zxf >FILE<" \
+    *.tar.gz > $out/$commit.out &&\
+  rm *.tar.gz
+done
+cd -
+rm -rf $temporary_repo
