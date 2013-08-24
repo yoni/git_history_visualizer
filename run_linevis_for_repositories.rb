1668195f25665a7d674a48a4520369ac3050dd78
Yoni Ben-Meshulam <yoni.bmesh@gmail.com>
2013-08-24 13:03:05 -0400

    Remove redundant comments.

--- run_linevis_for_repositories.rb
+++ run_linevis_for_repositories.rb
@@ -2,11 +2,9 @@
 require 'open-uri'
 require 'json'
 
-# JSON file of repositories. See get_all_repositories.rb
 repositories_file = ARGV[0]
 pattern = ARGV[1]
 path = ARGV[2]
-
 
 repositories = JSON.parse(open(repositories_file).read)
 selected_repositories = repositories.select{|repository| repository['name'] =~ /#{pattern}/ }
